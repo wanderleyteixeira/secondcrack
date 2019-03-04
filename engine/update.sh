ec9660df2fbc51055450655e6b7ad53573b11449
Wanderley Teixeira <wbtf@users.noreply.github.com>
2019-03-03 20:39:56 -0500

    Added pushover notifications and update tail command for Alpine

--- engine/update.sh
+++ engine/update.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ash
 
 if [ "$1" == "" -o "$2" == "" ] ; then
     echo ""
@@ -12,19 +12,21 @@ fi
 SOURCE_PATH="$1"
 SECONDCRACK_PATH="$2"
 FORCE_CHECK_EVERY_SECONDS=30
-UPDATE_LOG=/tmp/secondcrack-update.log
+#UPDATE_LOG=/tmp/secondcrack-update.log
+UPDATE_LOG="${SECONDCRACK_PATH}/secondcrack-update.log"
+
 #Set update_log_max < 1 for Infinite logfile length
 UPDATE_LOG_MAX=99
 
 SCRIPT_LOCK_FILE="${SECONDCRACK_PATH}/engine/secondcrack-updater.pid"
-BASH_LOCK_DIR="${SECONDCRACK_PATH}/engine/secondcrack-updater.sh.lock"
+BASH_LOCK_DIR="/tmp/secondcrack-updater.sh.lock"
 
 if mkdir "$BASH_LOCK_DIR" ; then
     trap "rmdir '$BASH_LOCK_DIR' 2>/dev/null ; exit" INT TERM EXIT
 
     echo "`date` -- updating secondcrack" >> $UPDATE_LOG
     if [ $UPDATE_LOG_MAX -gt 0 ]; then
-        tail --lines $UPDATE_LOG_MAX $UPDATE_LOG > ${UPDATE_LOG}.tmp
+        tail -n $UPDATE_LOG_MAX $UPDATE_LOG > ${UPDATE_LOG}.tmp
 	mv ${UPDATE_LOG}.tmp $UPDATE_LOG
     fi
 
@@ -35,13 +37,16 @@ if mkdir "$BASH_LOCK_DIR" ; then
             inotifywait -q -q -r -t $FORCE_CHECK_EVERY_SECONDS -e close_write -e create -e delete -e moved_from "$SOURCE_PATH"
             if [ $? -eq 0 ] ; then
                 echo "`date` -- updating secondcrack, a source file changed" >> $UPDATE_LOG
+				/home/secondcrack/pushover.sh "Updating secondcrack, a source file changed" >> $UPDATE_LOG
             else
                 echo "`date` -- updating secondcrack, $FORCE_CHECK_EVERY_SECONDS seconds elapsed" >> $UPDATE_LOG
+				#/home/secondcrack/pushover.sh "Updating secondcrack, $FORCE_CHECK_EVERY_SECONDS seconds elapsed" >> $UPDATE_LOG
             fi
-            
+
             php -f "${SECONDCRACK_PATH}/engine/update.php" "$SCRIPT_LOCK_FILE"
-            while [ $? -eq 2 ] ; do 
-                echo "`date` -- updating secondcrack, last run performed writes" >> $UPDATE_LOG
+            while [ $? -eq 2 ] ; do
+                echo "-- updating secondcrack, last run performed writes" >> $UPDATE_LOG
+                /home/secondcrack/pushover.sh "Updating secondcrack, last run performed writes" >> $UPDATE_LOG
                 php -f "${SECONDCRACK_PATH}/engine/update.php" "$SCRIPT_LOCK_FILE"
             done
         done
